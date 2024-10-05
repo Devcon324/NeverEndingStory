8a9b05ab9428a8a8929db09f42838f8f10f780b4
Devon <63375190+Devcon324@users.noreply.github.com>
2024-10-04 21:19:44 -0400

    working prototype

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from groq import Groq
 from dotenv import load_dotenv
 from datetime import datetime
+import time
 load_dotenv()
 from src.firstWrite import firstWrite
 from src.nextWrite import nextWrite
@@ -9,28 +10,29 @@ from src.prevStory import getLastStoryChunk
 from src.writeToFile import writeToFile
 from src.debugOutput import debugOutput
 
-PATH_TO_README = "../test.md"
+PATH_TO_README = "./test.md"
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"),)
 
-first_story  = firstWrite(client)
-first_output = first_story.choices[0].message.content
-first_date   = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def startStory(client):
+  first_story  = firstWrite(client)
+  first_output = first_story.choices[0].message.content
+  first_date   = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  debugOutput(first_story, first_date)
+  writeToFile(first_output, first_date, PATH_TO_README)
 
-debugOutput(first_story, first_date)
-writeToFile(first_output, first_date, PATH_TO_README)
+def writeNextStory(client):
+  last_story_chunk = getLastStoryChunk(PATH_TO_README)
+  if last_story_chunk:
+      print("\nLast story chunk retrieved:")
+      print(last_story_chunk, "\n")
+  else:
+      print("No previous story chunk found.")
+  next_story  = nextWrite(client, last_story_chunk)
+  next_output = next_story.choices[0].message.content
+  next_date   = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  debugOutput(next_story, next_date)
+  writeToFile(next_output, next_date, PATH_TO_README)
 
-last_story_chunk = getLastStoryChunk(PATH_TO_README)
-
-if last_story_chunk:
-    print("Last story chunk retrieved:")
-    print(last_story_chunk)
-else:
-    print("No previous story chunk found.")
-
-next_story  = nextWrite(client, last_story_chunk)
-next_output = next_story.choices[0].message.content
-next_date   = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-debugOutput(next_story, next_date)
-writeToFile(next_output, next_date, PATH_TO_README)
+startStory(client)
+writeNextStory(client)
--- src/writeToFile.py
+++ src/writeToFile.py
@@ -3,6 +3,9 @@ def writeToFile(output, date, file_path):
   # Open the README.md file in append mode and add the output to the end
   with open(file_path, "a") as file:
     file.write("\n")  # Add double line breaks between each output part
-    file.write(f"---\n**Date Written:** {date}\n\n")  # Add a separator and timestamp
+    file.write(f"---\n\n**Date Written:** {date}\n\n")  # Add a separator and timestamp
     file.write(output)
-    file.write("\n")  # Add double line breaks between each output part
+    file.write("\n")
+    print("Done writing to ", file_path)
+    print("Closing ", file_path)
+    file.close()  # Add double line breaks between each output part
